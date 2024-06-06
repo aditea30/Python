3584bcd8ac8ab9e82ff588d9bcd458ca8e5c7770
Aditi <aditi3004singh@gmail.com>
2024-06-06 19:59:44 +0530

    With and without zip

--- zip.py
+++ zip.py
@@ -2,4 +2,8 @@
 l1=[1,3,5,7,9]
 l2=['January','March','May','July','September']
 for a,b in zip(l1,l2):
-    print(a ,b)
+    print(a ,b,"\n")
+#Without using zip
+t=len(l1)
+for i in range(t):
+    print(l1[i],l2[i])
