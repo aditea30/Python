7eb528666fe5cfbecbba7dddffd16088f01890e2
Aditi <aditi3004singh@gmail.com>
2024-06-08 18:31:30 +0530

    Functions in tuple

---
+++ dict.py
@@ -0,0 +1,3 @@
+d={'1':{'class':'8','fees':'8000'},'3':'March'}
+for r in d['1']['class']:
+    print(r)
---
+++ tuple.py
@@ -0,0 +1,12 @@
+#tuple functions
+a=(10,56,43,52,10)
+s=min(a)
+print(s)
+p=max(a)
+print(p)
+i=a.index(43)
+print(i)
+c=a.count(10)
+print(c)
+l=sum(a,2)
+print(l)
