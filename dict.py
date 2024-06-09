c71ec89227ecb1f51239cf0779b6a6bb05fdea13
Aditi <aditi3004singh@gmail.com>
2024-06-09 17:00:15 +0530

    Dictionary and set functions

--- dict.py
+++ dict.py
@@ -1,11 +1,17 @@
 #dictionary 
-d={'1':{'class':'8','fees':'8000'},'3':'March'}
+'''d={'1':{'class':'8','fees':'8000'},'3':'March'}
 for r in d['1']['class']:
-    print(r)
+    print(r)'''
 
 #set functions
 s=(2,5,34,78)
 s1=set(s)
 print(s1)
 s1.add(72)
+print(s1)
+s1.pop()
+print(s1)#pops the first element
+s1.remove(34)#gives an error if item to be removed does'nt exsist
+print(s1)
+s1.discard(2)#does'nt give an error
 print(s1)
